db9fc9959123637229219a6dc2fb655e32f256e2
RamirezBB <brbrayan1945@gmail.com>
2023-10-27 19:51:01 -0600

    add Fragments

--- part8/react-graphql/src/persons/graphql-queries.js
+++ part8/react-graphql/src/persons/graphql-queries.js
@@ -1,15 +1,23 @@
 import { gql } from '@apollo/client'
 
+export const PERSON_ALL_DETAILS_FRAGMENT = gql`
+  fragment PersonDetails on Person {
+    name
+    id
+    phone
+    address {
+      city
+      street
+    }
+  }
+`
+
 export const ALL_PERSONS = gql`
   query getPersons {
     allPersons {
-      name
-      id
-      phone
-      address {
-        city
-        street
-      }
+      ...PersonDetails
     }
   }
+
+  ${PERSON_ALL_DETAILS_FRAGMENT}
 `
